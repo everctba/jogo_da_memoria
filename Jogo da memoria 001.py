7ea27ffd70c178d52f4a6225cfb4547b9bd91b52
everctba <everson777@gmail.com>
2024-04-20 20:12:12 -0300

    Update Jogo da memoria 001.py
    adicionado gameloop

--- Jogo da memoria 001.py
+++ Jogo da memoria 001.py
@@ -13,13 +13,14 @@ def printa_matriz():
     print(matriz[4]["card"], matriz[5]["card"],matriz[6]["card"],matriz[7]["card"])
     #print(matriz[8]["card"],matriz[9]["card"],matriz[10]["card"],matriz[11]["card"])
     print ('') 
+    
+def menu_instrucoes():
+    #Menu game instruções  
+    print('\n'*50)
+    print ('------------------------------------')
+    print ('C = Linguagem C, P = Python, R = Rust, J = Java, K = Korlin')
+    print ('')
 
-
-#Menu game instruções  
-print('\n'*50)
-print ('------------------------------------')
-print ('C = Linguagem C, P = Python, R = Rust, J = Java, K = Korlin * = BOMB!')
-print ('')
 
 
 
@@ -32,7 +33,7 @@ card2 = {"card":3,"linguagem":'P'}
 card3 = {"card":4,"linguagem":"R"}
 card4 = {"card":5,"linguagem":"R"}
 card5 = {"card":6,"linguagem":"P"}
-card6 = {"card":7,"linguagem":"J"}
+card6 = {"card":7,"linguagem":"K"}
 card7 = {"card":8,"linguagem":"K"}
 
 matriz.append(card0)
@@ -45,60 +46,59 @@ matriz.append(card6)
 matriz.append(card7)
 
 
+while True:
+    menu_instrucoes()
+    printa_matriz()
 
 
-printa_matriz()
-
-
-# MENU DE IMPUT de CARTA
-carta_1 = input("Escolha PRIMEIRA carta: ")
-print (carta_1)
-carta_1_em_int = int(carta_1)
-carta_1_em_int = carta_1_em_int-1
-print ('')
-print (carta_1_em_int)
-print ('')
-print ("carta", carta_1_em_int+1, "é:",  matriz[carta_1_em_int]["linguagem"])
-print ('')
-
-
-carta_2 = input("Escolha SEGUNDA carta: ")
-
-carta_2_em_int = int(carta_2)
-carta_2_em_int = carta_2_em_int-1
-print ('')
-print (carta_2_em_int)
-print ('')
-print ("carta", carta_2_em_int+1, "é:", matriz[carta_2_em_int]["linguagem"])
-print ('')
-
-#print Resultado de comparação
-resultado1 = matriz[carta_1_em_int]["linguagem"]
-resultado2 = matriz[carta_2_em_int]["linguagem"]
-
-str(resultado1)
-str(resultado2)
-
-print (resultado1 + " & " + resultado2)
-
-
-if  resultado1 == resultado2:
-    print ('Parabéns, você ACERTOU!')
+    # MENU DE IMPUT de CARTA
+    carta_1 = input("Escolha PRIMEIRA carta: ")
+    print (carta_1)
+    carta_1_em_int = int(carta_1)
+    carta_1_em_int = carta_1_em_int-1
     print ('')
-    #print ('Escolha novas cartas') 
-    print ('------------------------')
-else:
-    print ('você ERROU!')
-    print (' ')
-    print ('Tente novamente nesta próxima jogada') 
-    print (' ---------------------------------')
+    print (carta_1_em_int)
+    print ('')
+    print ("carta", carta_1_em_int+1, "é:",  matriz[carta_1_em_int]["linguagem"])
+    print ('')
 
-  
-#time.sleep(40)
 
-#limpa_tela()
-#print ('tempo sleep passou!')
-#time.sleep(4)
+    carta_2 = input("Escolha SEGUNDA carta: ")
 
+    carta_2_em_int = int(carta_2)
+    carta_2_em_int = carta_2_em_int-1
+    print ('')
+    print (carta_2_em_int)
+    print ('')
+    print ("carta", carta_2_em_int+1, "é:", matriz[carta_2_em_int]["linguagem"])
+    print ('')
+
+    #print Resultado de comparação
+    resultado1 = matriz[carta_1_em_int]["linguagem"]
+    resultado2 = matriz[carta_2_em_int]["linguagem"]
+
+    str(resultado1)
+    str(resultado2)
+    ('------------------------')
+    print (resultado1 + " & " + resultado2)
+
+
+    if  resultado1 == resultado2:
+        print ('Parabéns, você ACERTOU!')
+        print ('')
+        #print ('Escolha novas cartas') 
+        print ('------------------------')
+    else:
+        print ('Você ERROU!')
+        print (' ')
+        print ('Tente novamente nesta próxima jogada') 
+        print (' ---------------------------------')
 
     
+    time.sleep(4)
+
+    limpa_tela()
+    #print ('tempo sleep passou!')
+    #time.sleep(4)
+
+
