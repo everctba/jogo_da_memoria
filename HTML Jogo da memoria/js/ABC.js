ac796e3bb7b49dd14f6ba061b1a8e7f8271e86e6
everctba <everson777@gmail.com>
2024-05-01 21:56:53 -0300

    criado base pagina ABC
    grid do abc com craição dinamica das imagens

---
+++ HTML Jogo da memoria/js/ABC.js
@@ -0,0 +1,55 @@
+const grid = document.querySelector(".grid-abc");
+
+
+
+
+const frutas = [
+   '0',
+   '1',
+   '2',
+   '3',
+   '4',
+   '5',
+   'a',
+   'b',
+   'c',
+   'd',
+   'e',
+   'f',
+];
+
+
+const criaDiv = (tag, className) => {
+   const element = document.createElement(tag);
+   element.className = className;
+   return element;
+}
+
+const createCard = (frente) => {
+   const card = criaDiv('div','tamanho-carta');
+   const cartaIlustrada = criaDiv('div',' card face verso-carta');
+ 
+   cartaIlustrada.style.backgroundImage = `url("./images/abc/${frente}.jpg")`;
+   card.appendChild(cartaIlustrada);
+   grid.appendChild(card);
+   
+   // card.addEventListener('click', revelaCarta);
+   // card.setAttribute('data-frutas', frente);
+   return card;
+}
+
+
+
+
+
+
+const carregaCartas = () => {
+  frutas.forEach((frente) => {
+      const card = createCard(frente);
+      grid.appendChild(card);
+   })   
+   
+}
+carregaCartas();
+createCard(frutas);
+console.log(grid);
