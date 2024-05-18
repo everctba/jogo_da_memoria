da1e3d9cad7fdd08b61aed9dccf3bf150b21728d
everctba <everson777@gmail.com>
2024-05-18 20:09:54 -0300

    avançado na seleção das cartas e sons
    avançado na seleção das cartas

--- HTML Jogo da memoria/js/ABC.js
+++ HTML Jogo da memoria/js/ABC.js
@@ -3,6 +3,7 @@ const grid = document.querySelector(".grid-abc");
 let  audioA = new Audio("../HTML%20Jogo%20da%20memoria/som/abc/a.mp3");
 let  audioB = new Audio("../HTML%20Jogo%20da%20memoria/som/abc/b.mp3");
 let  audioC = new Audio("../HTML%20Jogo%20da%20memoria/som/abc/c.mp3");
+let  audioD = new Audio("../HTML%20Jogo%20da%20memoria/som/abc/d.mp3");
 
 
 const abcCartas = [
@@ -45,6 +46,12 @@ const abcCartas = [
    
 ];
 
+printE = ({target}) => {
+   console.log("entrou no click do botao add event listener");
+   console.log(target);
+}
+
+
 
 const criaDiv = (tag, className) => {
    const element = document.createElement(tag);
@@ -70,6 +77,7 @@ const carregaCartas = () => {
    abcCartas.forEach((frente) => {
       const card = createCard(frente);
       grid.appendChild(card);
+      card.setAttribute('data-carta', frente);
    })   
    
 }
@@ -78,7 +86,33 @@ createCard(abcCartas);
 
 
 
-console.log(grid);
+// console.log(grid);
+var clicadoAgora = document.querySelectorAll("data-carta");
+console.log(clicadoAgora);
+// var clicadoAgora = document.getElementsByClassName("tamanho-carta");
+// console.log(clicadoAgora);
+
+// for (var i = 0; i < clicadoAgora.length; i++) {
+   
+//    clicadoAgora[i].addEventListener("click", printE);
+
+//    console.log("clicadoAgora i  = ", clicadoAgora[i]);
+
+// }
+
+
+
+// clicadoAgora.addEventListener('click', function(e){
+//    console.log("foi clicado no ", e);
+// }); 
+// clicado.addEventListener("click", printE);
+
+// clicadoAgora.dataset.addEventListener("click", printE);
+// if (clicadoAgora.dataset.carta === "a") {
+//    console.log("foi clicado no ", e);
+// };
+
+
 audioA.play();
 audioB.play();
 audioC.play();
