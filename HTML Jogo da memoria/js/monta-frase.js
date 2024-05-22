4e696487d9e0903f651630230d5c68fb8d5cd627
everctba <everson777@gmail.com>
2024-05-22 04:39:07 -0300

    adicionado legenda ao monta frase

--- HTML Jogo da memoria/js/monta-frase.js
+++ HTML Jogo da memoria/js/monta-frase.js
@@ -1,5 +1,9 @@
+//define os grids específicos
 const grid01 = document.querySelector(".grid-monta-frase-01");
+const grid02 = document.querySelector(".grid-monta-frase-02");
 
+
+//serie de cartas
 const Cartas01 = [
    'cheiro',
    'falo',
@@ -9,37 +13,104 @@ const Cartas01 = [
    'vejo'
 ];
 
+const Cartas02 = [
+   'calor',
+   'sorvete',
+   'pipa',
+   'correr',
+   'bicicleta',
+   'quebracabeca',
+   'tocar',
+   'pintar',
+   'estudar',
+   'brincar',
+   'cantar',
+   'pular',
+   'basquete',
+   'futebol',
+   'videogame',
+   'fruta',
+   'musica',
+   'ler'
+
+];
+
+
+
 const criaDiv = (tag, className) => {
    const element = document.createElement(tag);
    element.className = className;
+   // element.createTextNode("titulo");
    return element;
 }
 
-const createCard = (frente) => {
+const createCard01 = (frente) => {
    const card = criaDiv('div','tamanho-carta');
    const cartaIlustrada = criaDiv('div',' card face verso-carta');
- 
+   
+   var textoCarta = criaDiv('text','texto-legenda');
+   var conteudoTexto = document.createTextNode("titulo");// textoCarta = document.createTextNode("titulo");
+   textoCarta.appendChild(conteudoTexto);
+   
    cartaIlustrada.style.backgroundImage = `url("./images/monta_frase/01/${frente}.jpg")`;
+   
+   // console.log(textoCarta);
+   
+   const textoCarta1 = document.createTextNode('titulo');
+   
+   cartaIlustrada.appendChild(textoCarta);
    card.appendChild(cartaIlustrada);
+   // card.appendChild(textoCarta);
+   // card.appendChild(textoCarta);
    grid01.appendChild(card);
-  
+
+   const txtLegenda = document.getElementsByTagName('text');
+   // txtLegenda.createTextNode("titulo");
+   // textoCarta1.appendChild(txtLegenda);
    return card;
 }
 
-const carregaCartas = () => {
+const createCard02 = (frente) => {
+   const card = criaDiv('div','tamanho-carta');
+   const cartaIlustrada = criaDiv('div',' card face verso-carta');
+  
+   cartaIlustrada.style.backgroundImage = `url("./images/monta_frase/02/${frente}.jpg")`;
+   card.appendChild(cartaIlustrada);
+   grid02.appendChild(card);
+  
+   return card;
+}
+const carregaCartas01 = () => {
    Cartas01.forEach((frente) => {
-      const card = createCard(frente);
+      const card = createCard01(frente);
       grid01.appendChild(card);
       card.setAttribute('data-carta', frente);
-   }) 
-   // for (var i = 0, len = Cartas01.length; i < len-1; ++i) {
-   //    const card = createCard(Cartas01[i]);
-   //    grid01.appendChild(card);
-   //    card.setAttribute('data-carta', Cartas01[i]);
-   // }
-   
+   })   
 }
-carregaCartas();
-createCard(Cartas01);
+const carregaCartas02 = () => {
+   Cartas02.forEach((frente) => {
+      const card = createCard02(frente);
+      grid02.appendChild(card);
+      card.setAttribute('data-carta', frente);
+   })   
+}
 
-Cartas01.forEach((element) => console.log(element));
+// const criaDivLinha = () => {
+//    const element = document.createElement('div');
+//    element.className = ('centraliza-linha', 'linha-montar-frase');
+  
+//    element.style.backgroundImage = `url("./images/menu/linha3.png")`;
+//    console.log("criadiv");
+//    grid01.appendChild(element);
+//    return element;
+// }
+
+
+carregaCartas01();
+createCard01(Cartas01);
+
+carregaCartas02();
+createCard02(Cartas02);
+
+
+// Cartas01.forEach((element) => console.log(element));
