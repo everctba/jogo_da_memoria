50c86aec44b8c9e97d959aeb524cf9f498bb21ca
everctba <everson777@gmail.com>
2024-06-07 23:00:30 -0300

    adicionado animação linhas no menu

---
+++ HTML Jogo da memoria/js/menuBodymoving.js
@@ -0,0 +1,46 @@
+
+
+
+const botao = document.querySelector('.quadrado');
+// var anim2 = document.querySelector('.linha-1-anima');
+var anim2 = document.getElementsByClassName("linha-1-anima");
+
+var itemAnima = lottie.loadAnimation({
+   wrapper: anim2[0],
+   animType: 'svg',
+   loop: true,
+   autoplay: false,
+   path:  './js/linha.json'
+});
+itemAnima.goToAndPlay(0,true);
+console.log(anim2);
+var itemAnima = lottie.loadAnimation({
+   wrapper: anim2[1],
+   animType: 'svg',
+   loop: true,
+   autoplay: false,
+   path:  './js/linha.json'
+});
+itemAnima.goToAndPlay(0,true);
+var itemAnima = lottie.loadAnimation({
+   wrapper: anim2[2],
+   animType: 'svg',
+   loop: true,
+   autoplay: false,
+   path:  './js/linha.json'
+});
+itemAnima.goToAndPlay(0,true);
+var itemAnima = lottie.loadAnimation({
+   wrapper: anim2[3],
+   animType: 'svg',
+   loop: true,
+   autoplay: false,
+   path:  './js/linha.json'
+});
+itemAnima.goToAndPlay(0,true);
+// anim.addEventListener('click', () => {
+// });
+// botao.addEventListener('click', () => {
+//    console.log(botao);
+//    itemAnima.goToAndPlay(0,true);
+// });
