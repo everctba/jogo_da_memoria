6210bb99fe4c19b2bd191f6270af5ed5b33ac4ad
everctba <everson777@gmail.com>
2024-06-13 13:58:29 -0300

    adicionado nuvens animadas

--- HTML Jogo da memoria/js/menuBodymoving.js
+++ HTML Jogo da memoria/js/menuBodymoving.js
@@ -1,5 +1,25 @@
 
 
+var nuvenAnima = document.getElementsByClassName("anima-nuven");
+var nuvenANIMADA = lottie.loadAnimation({
+   wrapper: nuvenAnima[0],
+   animType: 'svg',
+   loop: true,
+   autoplay: true,
+   path:  './js/nuvens-anima.json'
+});
+nuvenANIMADA.goToAndPlay(0,true);
+console.log(nuvenAnima);
+var nuvenAnima2 = document.getElementsByClassName("nuven2");
+var nuvenANIMADA2 = lottie.loadAnimation({
+   wrapper: nuvenAnima2[0],
+   animType: 'svg',
+   loop: true,
+   autoplay: true,
+   path:  './js/nuvens-anima2.json'
+});
+nuvenANIMADA2.goToAndPlay(0,true);
+console.log(nuven2);
 
 const botao = document.querySelector('.quadrado');
 // var anim2 = document.querySelector('.linha-1-anima');
