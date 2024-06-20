1b463352fcb862dfcc684a0e28d009cfa1a5dbae
everctba <everson777@gmail.com>
2024-06-20 00:36:09 -0300

    atualizado pato e porco

--- HTML Jogo da memoria/js/bodymoving-qual-o-animal.js
+++ HTML Jogo da memoria/js/bodymoving-qual-o-animal.js
@@ -11,4 +11,17 @@ var itemAnima3 = lottie.loadAnimation({
 console.log(anim3);
 
 // itemAnima3.goToAndPlay(0,true);
+var anim4 = document.getElementsByClassName("confeti-porco");
+
+
+var itemAnima4 = lottie.loadAnimation({
+   wrapper: anim4[0],
+   animType: 'svg',
+   loop: true,
+   autoplay: true,
+   path:  './js/data.json'
+});
+console.log(anim4);
+
+// itemAnima3.goToAndPlay(0,true);
 
--- HTML Jogo da memoria/js/qual-o-animal.js
+++ HTML Jogo da memoria/js/qual-o-animal.js
@@ -2,9 +2,14 @@ let pato = document.getElementsByClassName("pato");
 let opcaoPato1 = document.getElementsByClassName("opcao-pato-1");
 let opcaoPato2 = document.getElementsByClassName("opcao-pato-2");
 let opcaoPato3 = document.getElementsByClassName("opcao-pato-3");
-let confeti = document.getElementsByClassName("confeti-pato");
+let confetiPato = document.getElementsByClassName("confeti-pato");
 
 let porco = document.getElementsByClassName("porco");
+let opcaoPorco1 = document.getElementsByClassName("opcao-porco-1");
+let opcaoPorco2 = document.getElementsByClassName("opcao-porco-2");
+let opcaoPorco3 = document.getElementsByClassName("opcao-porco-3");
+let confetiPorco = document.getElementsByClassName("confeti-porco");
+
 let frango = document.getElementsByClassName("frango");
 let vaca = document.getElementsByClassName("vaca");
 let ovelha = document.getElementsByClassName("ovelha");
@@ -26,39 +31,46 @@ let card_menu_gato = document.getElementById("card-menu-gato")
 abre_menu_pato = ({ target }) => {
    console.log("Entrou no abre menu PATO  = ", pato);
    card_menu_pato.style.display = "flex"; 
+   setTimeout(() => {
+      card_menu_pato.style.opacity = "100%";  
+   } ,350);
 }
 
 abre_menu_porco = ({ target }) => {
    console.log("Entrou no abre menu PORCO = ", porco);
-   card_menu_pato.style.display = "flex";
+   console.log("card_menu_porco = ", card_menu_porco);
+   card_menu_porco.style.display = "flex";
+   setTimeout(() => {
+      card_menu_porco.style.opacity = "100%";  
+   } ,350);
 }
 abre_menu_frango = ({ target }) => {
    console.log("Entrou no abre menu FRANGO = ", frango);
-   card_menu_pato.style.display = "flex";
+   card_menu_frango.style.display = "flex";
 }
 abre_menu_vaca = ({ target }) => {
    console.log("Entrou no abre menu VACA = ", vaca);
-   card_menu_pato.style.display = "flex";
+   card_menu_vaca.style.display = "flex";
 }
 abre_menu_ovelha = ({ target }) => {
    console.log("Entrou no abre menu OVELHA = ", ovelha);
-   card_menu_pato.style.display = "flex";
+   card_menu_ovelha.style.display = "flex";
 }
 abre_menu_cavalo = ({ target }) => {
    console.log("Entrou no abre menu CAVALO = ", cavalo);
-   card_menu_pato.style.display = "flex";
+   card_menu_cavalo.style.display = "flex";
 }
 abre_menu_coelho = ({ target }) => {
    console.log("Entrou no abre menu COELHO = ", coelho);
-   card_menu_pato.style.display = "flex";
+   card_menu_coelho.style.display = "flex";
 }
 abre_menu_bode = ({ target }) => {
    console.log("Entrou no abre menu BODE = ", bode);
-   card_menu_pato.style.display = "flex";
+   card_menu_bode.style.display = "flex";
 }
 abre_menu_gato = ({ target }) => {
    console.log("Entrou no abre menu GATO = ", gato);
-   card_menu_pato.style.display = "flex";
+   card_menu_gato.style.display = "flex";
 }
 
 pato[0].addEventListener('click', abre_menu_pato);
@@ -74,9 +86,16 @@ gato[0].addEventListener('click', abre_menu_gato);
 
 opcaoCertaPato = ({ target }) => {
    console.log("Opção certa clicada  = ", opcaoPato1);
-   console.log("confeti[0] = ", confeti[0]);
-   confeti[0].style.display = "flex";
+   console.log("confeti[0] = ", confetiPato[0]);
+   confetiPato[0].style.display = "flex";
    opcaoPato1[0].classList.add("certo-clicado");
+   setTimeout(() => {
+      card_menu_pato.style.opacity = "0%";  
+   } ,150);
+   setTimeout(() => {
+      card_menu_pato.style.display = "none";   
+   } ,450);
+
 }
 opcaoErradaPato1 = ({ target }) => {
    console.log("Opção ERRADA clicada  = ", opcaoPato2);
@@ -94,7 +113,40 @@ opcaoErradaPato2 = ({ target }) => {
    opcaoPato3[0].classList.remove("errado");
 
 }
+opcaoCertaPorco = ({ target }) => {
+   console.log("Opção certa clicada  = ", opcaoPorco1);
+   console.log("confeti[0] = ", confetiPorco[0]);
+   confetiPorco[0].style.display = "flex";
+   opcaoPorco1[0].classList.add("certo-clicado");
+   
+   setTimeout(() => {
+      card_menu_porco.style.opacity = "0%";  
+   } ,150);
+   setTimeout(() => {
+      card_menu_porco.style.display = "none";   
+   } ,450);
+}
+opcaoErradaPorco1 = ({ target }) => {
+   console.log("Opção ERRADA clicada  = ", opcaoPorco2);
+   opcaoPorco2[0].classList.add("opcao-pos-clique");
+   opcaoPorco2[0].classList.add("errado-clicado");
+   opcaoPorco2[0].classList.remove("opcao");
+   opcaoPorco2[0].classList.remove("errado");
+
+}
+opcaoErradaPorco2 = ({ target }) => {
+   console.log("Opção ERRADA clicada  = ", opcaoPorco3);
+   opcaoPorco3[0].classList.add("opcao-pos-clique");
+   opcaoPorco3[0].classList.add("errado-clicado");
+   opcaoPorco3[0].classList.remove("opcao");
+   opcaoPorco3[0].classList.remove("errado");
+
+}
 opcaoPato1[0].addEventListener('click', opcaoCertaPato);
 opcaoPato2[0].addEventListener('click', opcaoErradaPato1);
 opcaoPato3[0].addEventListener('click', opcaoErradaPato2);
+
+opcaoPorco1[0].addEventListener('click', opcaoCertaPorco);
+opcaoPorco2[0].addEventListener('click', opcaoErradaPorco1);
+opcaoPorco3[0].addEventListener('click', opcaoErradaPorco2);
 
